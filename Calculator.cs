17e3ba75f74c5f446eeb16b8c86b1f4db2d8522a
Bela Patel <belapatel1995@gmail.com>
2024-07-10 14:56:17 +0100

    Initial Commit

---
+++ Calculator.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace AtlantaTechnical
+{
+    internal class Calculator
+    {
+        public void Execute()
+        {
+            decimal totalPrice = 0;
+
+            //Sample data
+            Dictionary<string,decimal> result = new Dictionary<string,decimal>();
+            result.Add("Cakes", Convert.ToDecimal(0.50));
+            result.Add("A loaf of bread", Convert.ToDecimal(1.50));
+            result.Add("An artisan loaf", Convert.ToDecimal(2.50));
+           
+            foreach (var key in result)
+            {
+               Console.WriteLine("Item name : " + key.Key);
+               totalPrice += key.Value;
+            }
+            Console.WriteLine("Total Cost : " + totalPrice);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AtlantaTechnical
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call method of display numbers
+            //var obj = new DisplayNumbers();
+            //obj.Execute();
+
+            // Call method of display items and total cost 
+            var obj = new Calculator();
+            obj.Execute();
+        }
+    }
+}
